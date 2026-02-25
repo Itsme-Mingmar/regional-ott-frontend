a22a93e23e81871b2902071292a1d35b43f01b05
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-02-25 17:31:26 +0545

    feat(homepage): homepage after login

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import ProvinceFilter from "../components/common/ProvinceFilter";
+import MovieGrid from "../components/common/MovieGrid";
+import ProvinceSection from "../components/common/ProvinceSection";
+
+const provinces = [
+  "Global Movies",
+  "Koshi",
+  "Madesh",
+  "Bagmati",
+  "Gandaki",
+  "Lumbini",
+  "Karnali",
+  "Sudurpashchim",
+];
+
+const HomePage = () => {
+  const [activeProvince, setActiveProvince] = useState("Global Movies");
+
+  return (
+    <div className="pt-24 px-6 pb-16 bg-[#0F0F1A] min-h-screen text-white">
+      <ProvinceFilter
+        provinces={provinces}
+        activeProvince={activeProvince}
+        setActiveProvince={setActiveProvince}
+      />
+
+      {activeProvince === "Global Movies" ? (
+        <MovieGrid />
+      ) : (
+        <ProvinceSection province={activeProvince} />
+      )}
+    </div>
+  );
+};
+
+export default HomePage;
