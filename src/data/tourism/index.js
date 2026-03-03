191719b4c2d546d0df26b37801bdffcd06d60a05
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-03-03 10:19:14 +0545

    feat(index): map all province data

--- src/data/tourism/index.js
+++ src/data/tourism/index.js
@@ -1,5 +1,17 @@
 import { gandakiTourism } from "./gandaki";
+import { koshiTourism } from "./koshi";
+import { madeshTourism } from "./madesh";
+import { bagmatiTourism } from "./bagmati";
+import { lumbiniTourism } from "./lumbini";
+import { karnaliTourism } from "./karnali";
+import { sudurTourism } from "./sudurpaschim";
 
 export const tourismData = {
   Gandaki: gandakiTourism,
+  Koshi: koshiTourism,
+  Madesh: madeshTourism,
+  Bagmati: bagmatiTourism,
+  Lumbini: lumbiniTourism,
+  Karnali: karnaliTourism,
+  Sudurpaschim: sudurTourism
 };
