1779aad1ec84c0e7f4fbaa0e019ab3944959ea8e
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-04-08 18:53:58 +0545

    feat(ProvinceFilter): save active province on localstorage

--- src/components/common/ProvinceFilter.jsx
+++ src/components/common/ProvinceFilter.jsx
@@ -16,16 +16,18 @@ const ProvinceFilter = ({
             <button
               key={province}
               onClick={() => {
-                if (!isDisabled) setActiveProvince(province);
+                if (!isDisabled)
+                  setActiveProvince(province);
+                  localStorage.setItem("activeProvince", province);
+
               }}
               disabled={isDisabled}
-              className={`px-6 py-2 rounded-full whitespace-nowrap transition-all duration-300 ${
-                activeProvince === province
+              className={`px-6 py-2 rounded-full whitespace-nowrap transition-all duration-300 ${activeProvince === province
                   ? "bg-linear-to-r from-purple-600 to-pink-500 text-white shadow-lg scale-105"
                   : isDisabled
-                  ? "bg-gray-700 text-gray-400 cursor-not-allowed"
-                  : "bg-[#1C1C2E] hover:bg-purple-700"
-              }`}
+                    ? "bg-gray-700 text-gray-400 cursor-not-allowed"
+                    : "bg-[#1C1C2E] hover:bg-purple-700"
+                }`}
             >
               {province}
             </button>
